e63cee580fce29ca76b8d1bf596f9de5f1aadf1a
MarianJandl <142720644+MarianJandl@users.noreply.github.com>
2025-02-11 10:45:09 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const bpmSlider = document.getElementById("bpmSlider");
     const bpmInput = document.getElementById("bpmInput");
+    
     document.getElementById("toggleBtn").addEventListener("click", toggleMetronome);
     document.getElementById("applyBtn").addEventListener("click", applySettings);
     //document.getElementById("resetBtn").addEventListener("click", resetToDefault);
@@ -12,6 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     bpmInput.addEventListener("input", function () {
         bpmSlider.value = bpmInput.value;
+    });
+
+    // Listen for "Enter" key press in bpmInput
+    bpmInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevent form submission if inside a form
+            const bpmValue = parseInt(bpmInput.value, 10);
+            if (!isNaN(bpmValue) && bpmValue > 0) {
+                updateBPM(bpmValue);
+            } else {
+                console.log("Invalid BPM value");
+            }
+        }
     });
 
     document.addEventListener("keydown", function(event) {
@@ -36,9 +50,7 @@ function updateBPM(bpmValue) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ bpm: bpmValue })
     })
-    .then(response => response.json())
-    
-    
+    .then(response => response.json());
 }
 
 function applySettings() {
@@ -48,9 +60,24 @@ function applySettings() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ bpm: bpmValue })
     })
-    .then(response => response.json())
-    
+    .then(response => response.json());
 }
+
+function checkServerStatus() {
+    $.get("/status", function(data) {
+        if (data.running) {
+            $("#serverStatus").text("Running").css("color", "lightgreen");
+        } else {
+            $("#serverStatus").text("Running").css("color", "lightgreen");
+        }
+    }).fail(function() {
+        $("#serverStatus").text("Disconnected").css("color", "red");
+    });
+}
+
+// Check server status every 1 second
+setInterval(checkServerStatus, 1000);
+checkServerStatus();
 
 /*function resetToDefault() {
     fetch("/reset", { method: "POST" })
